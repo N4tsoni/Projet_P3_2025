f8e0fbc0571bd4035f2c14fc231ee75ad7d11fb8
N4tsoni <sofian6433@hotmail.fr>
2025-12-16 13:29:10 +0100

    Ajout du frontend en tant que sous-module

--- frontend/src/components/atoms/index.ts
+++
@@ -1,6 +0,0 @@
-// Atoms - Export barrel file
-export { default as BaseButton } from './BaseButton.vue'
-export { default as BaseBadge } from './BaseBadge.vue'
-export { default as BaseIcon } from './BaseIcon.vue'
-export { default as BaseSpinner } from './BaseSpinner.vue'
-export { default as BaseAvatar } from './BaseAvatar.vue'
--- frontend/src/components/molecules/index.ts
+++
@@ -1,4 +0,0 @@
-// Molecules - Export barrel file
-export { default as StatCard } from './StatCard.vue'
-export { default as MessageBubble } from './MessageBubble.vue'
-export { default as AudioPlayer } from './AudioPlayer.vue'
